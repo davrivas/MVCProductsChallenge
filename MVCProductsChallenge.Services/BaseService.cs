0ed43a50e8e2e1ffca3e55427ac678e8d5195d65
David Rivas <david.rivas@pevaar.com>
2019-06-21 15:25:32 -0500

    Services created

---
+++ MVCProductsChallenge.Services/BaseService.cs
@@ -0,0 +1,23 @@
+﻿using MVCProductsChallenge.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProductsChallenge.Services
+{
+    public interface IBaseService<T>
+    {
+    }
+
+    public class BaseService
+    {
+        private readonly ProductsContext dbContext;
+
+        public BaseService()
+        {
+            dbContext = new ProductsContext();
+        }
+    }
+}
