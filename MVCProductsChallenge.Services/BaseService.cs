fc4b641895df40f2a30957288e950eb4177b7e62
David Rivas <david.rivas@pevaar.com>
2019-06-21 16:09:38 -0500

    Base service added

--- MVCProductsChallenge.Services/BaseService.cs
+++ MVCProductsChallenge.Services/BaseService.cs
@@ -1,4 +1,5 @@
 ﻿using MVCProductsChallenge.Context;
+using MVCProductsChallenge.Model.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,48 @@ namespace MVCProductsChallenge.Services
 {
     public interface IBaseService<T>
     {
+        void Create(T entity);
+        void Delete(T entity);
+        T Get(int id);
+        IQueryable<T> List();
+        void Update(T oldEntity, T newEntity);
     }
 
-    public class BaseService
+    public class BaseService<T> : IBaseService<T> where T : BaseEntity
     {
-        private readonly ProductsContext dbContext;
+        private readonly ProductsContext _dbContext;
 
         public BaseService()
         {
-            dbContext = new ProductsContext();
+            _dbContext = new ProductsContext();
+        }
+
+        public void Create(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public T Get(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> List()
+        {
+            return _dbContext.Set<T>().AsQueryable();
+        }
+
+        public void Update(T oldEntity, T newEntity)
+        {
+            _dbContext.Entry(oldEntity).CurrentValues.SetValues(newEntity);
+            _dbContext.SaveChanges();
         }
     }
 }
