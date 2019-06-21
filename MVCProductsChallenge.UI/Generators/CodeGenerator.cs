c02ae780b11075d3060ad2df3f41667a449b3135
David Rivas <david.rivas@pevaar.com>
2019-06-21 17:10:03 -0500

    Code Generator created (it may be added to another project)

---
+++ MVCProductsChallenge.UI/Generators/CodeGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProductsChallenge.UI.Generators
+{
+    public static class CodeGenerator
+    {
+        private const string CODE_CHARACTERS = "qwertyuioplkjhgfdsamnbvcxz";
+        private static readonly int _charactersLength = CODE_CHARACTERS.Length;
+        private static readonly Random random = new Random();
+
+        public static string GenerateCode(int maxCharacters = 5)
+        {
+            string code = string.Empty;
+
+            for (int i = 0; i < maxCharacters; i++)
+            {
+                int index = random.Next(_charactersLength);
+                code += CODE_CHARACTERS[index];
+            }
+
+            return code;
+        }
+    }
+}
