51439255cdb71d729293e669a3311f8d45452d05
David Rivas <david.rivas@pevaar.com>
2019-06-21 15:25:12 -0500

    Context created

---
+++ MVCProductsChallenge.Context/ProductsContext.cs
@@ -0,0 +1,20 @@
+﻿using MVCProductsChallenge.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProductsChallenge.Context
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext() : base(string.Empty)
+        {
+        }
+
+        public IDbSet<Product> Products { get; set; }
+        public IDbSet<ProductType> ProductTypes { get; set; }
+    }
+}
