1bd543b95748fb059b91cd80755a92e68f8214bd
David Rivas <david.rivas@pevaar.com>
2019-06-21 16:09:22 -0500

    toastr added

---
+++ MVCProductsChallenge.UI/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProductsChallenge.UI.Controllers
+{
+    public abstract class BaseController : Controller
+    {
+    }
+}
--- MVCProductsChallenge.UI/Controllers/HomeController.cs
+++ MVCProductsChallenge.UI/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace MVCProductsChallenge.UI.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
