9cb2f873c568f162ceeacc29cc33317764f955a9
David Rivas <david.rivas@pevaar.com>
2019-06-21 17:08:41 -0500

    View Models added

---
+++ MVCProductsChallenge.UI/ViewModels/HomeViewModels/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MVCProductsChallenge.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProductsChallenge.UI.ViewModels.HomeViewModels
+{
+    public sealed class IndexViewModel : BaseViewModel
+    {
+        public IList<Product> Products { get; set; }
+
+        public IndexViewModel()
+        {
+        }
+    }
+}
