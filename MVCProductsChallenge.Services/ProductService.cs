8bb15802ee3d5bad2645c9621d4198b428b2aafa
David Rivas <david.rivas@pevaar.com>
2019-07-03 08:54:05 -0500

    Check if identifier exists and highlight current page in navbar

--- MVCProductsChallenge.Services/ProductService.cs
+++ MVCProductsChallenge.Services/ProductService.cs
@@ -2,21 +2,34 @@
 using MVCProductsChallenge.Model.Enums;
 using MVCProductsChallenge.Services.Generators;
 using System;
+using System.Linq;
 
 namespace MVCProductsChallenge.Services
 {
     public interface IProductService : IBaseService<Product>
     {
+        bool ProductExists(string identifier);
     }
 
     public sealed class ProductService : BaseService<Product>, IProductService
     {
         public override void Create(Product entity)
         {
-            entity.Identifier = CodeGenerator.GenerateCode();
+            do
+            {
+                entity.Identifier = CodeGenerator.GenerateCode();
+            } while (ProductExists(entity.Identifier));
+
             entity.CreationDate = DateTime.Now;
             entity.ProductStatus = ProductStatus.Active;
             base.Create(entity);
+        }
+
+        public bool ProductExists(string identifier)
+        {
+            var product = List().Where(x => x.Identifier == identifier).FirstOrDefault();
+            bool productExists = product != null;
+            return productExists;
         }
     }
 }
